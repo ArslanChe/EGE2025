0f1390a0c9c77018e8924571f5a28e5202e74bc9
ArslanChe <113353936+ArslanChe@users.noreply.github.com>
2025-02-12 08:19:32 +0500

    Update 23.py

--- 23/23.py
+++ 23/23.py
@@ -9,8 +9,11 @@ from functools import lru_cache
 
 @lru_cache(maxsize=None)
 def f(s, t):
+    # Условие окончания подсчета/запрещенные поля
     if s < t or s == 28 : return 0
+    # Условие нужного пути
     if s==t: return 1
+    #Условие перехода
     if s % 2==0:
         return f(s//2, t) + f(s-2, t)
     else:
