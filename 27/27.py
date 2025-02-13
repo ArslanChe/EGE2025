6bb4478372149299da8709dc945e3be27626c2b0
ArslanChe <113353936+ArslanChe@users.noreply.github.com>
2025-02-13 06:14:30 +0500

    Дополнение задач

--- 27/27.py
+++ 27/27.py
@@ -1,18 +1,20 @@
 from math import *
-
-data = [tuple(map(int, f.split())) for f in open('27_A_18884.txt')]
+# №18678
+data = [tuple(map(float, f.split())) for f in open('27A_18678.txt')]
 # Аналитическое рабзиение на кластеры
 # Отбор аномалий происходит с помощью добавление условий выбора кластера
 clusters = [[],[]]
 for p in data:
-    if p[0] > 40: # Условие (строится по визуализации в экселе)
+    if (p[0] < 1 and p[1] >4) or (p[0] >6.5 and p[1]>2.5):
+        continue
+    elif  p[1] < 2: # Условие (строится по визуализации в экселе)
         clusters[0].append(p)
     else:
-        clusters[1].append(p[1])
+        clusters[1].append(p)
 for i in clusters:
     print(len(i))
 
-
+print()
 # Программное разбиение на кластеры
 # Отбор аномалий происходит с помощью изменения дистанции для выбора кластера
 clusters = []
@@ -20,7 +22,7 @@ while data:
     clusters.append([data.pop()])
     for p1 in clusters[-1]:
         for p2 in data[:]:
-            if dist(p1, p2) <10:
+            if dist(p1, p2) <1 :
                 clusters[-1].append(p2)
                 data.remove(p2)
 for i in clusters:
