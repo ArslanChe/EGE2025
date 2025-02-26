16bac96f785a7c170d96f98dbcfe61aebd7d14a9
ArslanChe <113353936+ArslanChe@users.noreply.github.com>
2025-02-26 14:52:17 +0300

    24

--- 24/24.py
+++ 24/24.py
@@ -1,6 +1,29 @@
-with open("24_318.txt") as f:
+# with open("24_318.txt") as f:
+#     s = f.read()
+#     for i in range(len(s)):
+#         if s[i].isalpha():
+#             s = s.replace(s[i], ' ')
+#     print(s.split())
+from re import *
+from fnmatch import fnmatch
+# with open("24_18285.txt") as f:
+#     s = f.read()
+#     num = r'(?:[1-9](?:[0-9])*)'
+#     pattern = rf'(?:{num}(?:[+*]{num})*)'
+#     prog = findall(pattern, s)
+#     ans = max([ len(list(x.replace('*', '+').split('+'))) for x in prog])
+#     print(ans)
+
+
+#9753
+with open("24_9753.txt") as f:
     s = f.read()
-    for i in range(len(s)):
-        if s[i].isalpha():
-            s = s.replace(s[i], ' ')
-    print(s.split())
+    r = l =0
+    c = 0
+    while l < len(s) and r < len(s):
+        if s[l:r].count("Y") <150:
+            r+= 1
+        else :
+            c = max(len(s[l:r]),c)
+            l+= 1
+    print(c)
