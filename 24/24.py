edf5081b62a7f1c8a57f80d1702b12c4c938ca26
ArslanChe <113353936+ArslanChe@users.noreply.github.com>
2025-04-10 10:27:57 +0300

    19884

--- 24/24.py
+++ 24/24.py
@@ -4,25 +4,25 @@
 #         if s[i].isalpha():
 #             s = s.replace(s[i], ' ')
 #     print(s.split())
-import re
-mx = 0
-with open("24_17641.txt") as f:
-    s = f.read()
-    num = r'(?:[1-9](?:[0-9])*|0)'
-    pattern = rf'(?:{num}(?:[+*]{num})*)'
-    prog = sorted(re.findall(pattern, s), reverse=True, key=len)
-    ans = [x for x in prog]
-    c = 0
-    test = r'[+][1-9]+[+]'
-    mx = 0
-    for x in ans:
-        if c == 20:
-            break
-        match = re.search(test, x)
-        tmp = []
-        print(x,len(x), match)
-        c+=1
-    print(mx)
+# import re
+# mx = 0
+# with open("24_17641.txt") as f:
+#     s = f.read()
+#     num = r'(?:[1-9](?:[0-9])*|0)'
+#     pattern = rf'(?:{num}(?:[+*]{num})*)'
+#     prog = sorted(re.findall(pattern, s), reverse=True, key=len)
+#     ans = [x for x in prog]
+#     c = 0
+#     test = r'[+][1-9]+[+]'
+#     mx = 0
+#     for x in ans:
+#         if c == 20:
+#             break
+#         match = re.search(test, x)
+#         tmp = []
+#         print(x,len(x), match)
+#         c+=1
+#     print(mx)
 # #
 # #9753
 # with open("24_9753.txt") as f:
@@ -50,3 +50,34 @@ with open("24_17641.txt") as f:
 #                 break
 #             l+= 1
 #     print(c)
+# def f(s):
+#     if len(s) == 1:
+#         return 1
+
+import re
+
+ans = 0
+def task(ex):
+    tmp = []
+    for i in range(len(ex)):
+        if ex[i] in '-*':
+            continue
+        elif ex[i] == '0' and i != len(ex)- 1 and ex[i+1] in '0123456789':
+            continue
+        else:
+            for j in range(len(ex) - 1, i, -1):
+                if ex[j] in '-*':
+                    continue
+                if ex[i:j].count('-') + ex[i:j].count('*') != 0:
+                    tmp.append(ex[i:j + 1])
+    return tmp
+
+with open("24_19884.txt") as f:
+    s = f.read()
+    num = r'(?:[6-9](?:[06789])*|0)'
+    pattern = rf'(?:{num}(?:[-*]{num})+)'
+
+    for exp in re.findall(pattern, s):
+        ans += len(task(exp))
+
+    print(ans)
