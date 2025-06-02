888fdcc4d3022fd3f03bdf550020c7f5d4b6838d
ArslanChe <113353936+ArslanChe@users.noreply.github.com>
2025-06-02 03:17:31 +0300

    22446

--- 24/24.py
+++ 24/24.py
@@ -82,22 +82,66 @@
 #
 #     print(ans)
 
-l = r = 0
-s = open('24_21717.txt').readline()
-ans = 1e10
-while l < len(s) and r < len(s):
-    if s[l:r+1].count('RSQ')<130:
-        r+= 1
-    elif s[l:r+1].count('RSQ')==130 and s[l:r+1][-1] != 'Q':
-        ans = min(ans,len(s[l:r+1]))
-        l+=1
-    elif s[l:r + 1].count('RSQ') == 130 and s[l:r + 1][-1] == 'Q':
-        r+=1
-    elif s[l:r + 1].count('RSQ') > 130:
-        l+= 1
+# l = r = 0
+# s = open('24_21717.txt').readline()
+# ans = 1e10
+# while l < len(s) and r < len(s):
+#     if s[l:r+1].count('RSQ')<130:
+#         r+= 1
+#     elif s[l:r+1].count('RSQ')==130 and s[l:r+1][-1] != 'Q':
+#         ans = min(ans,len(s[l:r+1]))
+#         l+=1
+#     elif s[l:r + 1].count('RSQ') == 130 and s[l:r + 1][-1] == 'Q':
+#         r+=1
+#     elif s[l:r + 1].count('RSQ') > 130:
+#         l+= 1
+#
+#     if l%100000==0:
+#         print(l,r)
+#         print(ans)
+#     if r > 9000576:
+#         print(l,r)
+# ans = 0
+# with open('24_13085.txt') as f:
+#     s =f.readline()
+#     for l in range(0,len(s)):
+#         for r in range(l+ans,len(s)):
+#             tmp = s[l:r+1]
+#             if tmp.count('X') + tmp.count('Y') == 2:
+#                 ans = max(ans, len(tmp))
+#             elif tmp.count('X')>1 or tmp.count('Y') >1:
+#                 break
+#     print(ans)
 
-    if l%100000==0:
-        print(l,r)
-        print(ans)
-    if r > 9000576:
-        print(l,r)
+# with open('24_20909.txt') as f:
+#     s = f.readline()
+#     m = 0
+#     for l in range(len(s)):
+#         print(l)
+#         for r in range(l + m, len(s)):
+#             slovo = s[l:r+1]
+#             if slovo.count('AB') == 100:
+#                 m = max(m, len(slovo))
+#             elif slovo.count('AB')< 100:
+#                 continue
+#             else:
+#                 break
+#     print(m)
+with open('24_22446.txt') as f:
+    s = f.readline()
+    m = 10000
+    for l in range(len(s)):
+        print(l)
+        for r in range(l + m, len(s)):
+            slovo = s[l:r+1]
+            lnd = slovo.count('LND')
+            if slovo[0]!= slovo[-1]:
+                continue
+            if slovo[0] == slovo[-1] and lnd == 10000:
+                m = max(m, len(slovo))
+            elif lnd< 10000:
+                continue
+            else:
+                break
+    print(m)
+
